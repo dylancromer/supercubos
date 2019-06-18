83350eeebd443756dd6ef0e398188c7239387221
dylancromer <dylanmattc@gmail.com>
2019-06-18 17:02:51 -0400

    Fixed horrifying deletion of my cython file

--- supercubos/csampling.pyx
+++ supercubos/csampling.pyx
@@ -1,26 +1,32 @@
+cimport numpy as np
 import numpy as np
-from _sampling import latin_sample as _sample
+ctypedef npfloat
 
 
 
+cdef np.ndarray[npfloat, ndim=2] latin_sample(np.ndarray[npfloat, ndim=1] param_mins,
+                                              np.ndarray[npfloat, ndim=1] param_maxes,
+                                              int num_samples):
+    ctypedef np.ndarray[npfloat, ndim=1] np_array_1d
+    ctypedef np.ndarray[npfloat, ndim=2] np_array_2d
 
-class LatinSampler:
-    def sample(self, param_mins, param_maxes, num_samples):
-        dim = param_mins.size
+    cdef int dim
+    dim = param_mins.size
 
-        hypercube_bins = np.array([np.random.permutation(num_samples) for i in range(dim)])
+    cdef np_array_1d hypercube_bins
+    hypercube_bins = np.array([np.random.permutation(num_samples) for i in range(dim)])
+    hypercube_bins = np.unique(hypercube_bins, axis=0)
+
+    cdef np_array_1d extra_point
+    while hypercube_bins.shape[0] < dim:
+        extra_point = np.random.permutation(num_samples)[None, :]
+        hypercube_bins = np.concatenate((hypercube_bins, extra_point), axis=0)
         hypercube_bins = np.unique(hypercube_bins, axis=0)
 
-        while hypercube_bins.shape[0] < dim:
-            extra_point = np.random.permutation(num_samples)[None, :]
-            hypercube_bins = np.concatenate((hypercube_bins, extra_point), axis=0)
-            hypercube_bins = np.unique(hypercube_bins, axis=0)
+    cdef np_array_1d lengths
+    lengths = param_maxes - param_mins
 
-        lengths = param_maxes - param_mins
+    cdef hypercube_points np_array_2d
+    hypercube_points = lengths*(hypercube_bins.T + 0.5)/num_samples
 
-        hypercube_points = lengths*(hypercube_bins.T + 0.5)/num_samples
-
-        return hypercube_points
-
-    def sample_cython(self, param_mins, param_maxes, num_samples):
-        return _sample(param_mins, param_maxes, num_samples)
+    return hypercube_points
