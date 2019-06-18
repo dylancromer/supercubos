18be200d61bc25af6ffcfe7fae6dc7d8dd8a6acd
dylancromer <dylanmattc@gmail.com>
2019-06-18 18:03:46 -0400

    Setup script works now

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
 
 
+
+extension = Extension("csampling", ["supercubos/csampling.pyx"], include_dirs=[numpy.get_include()])
 
 setup(name='supercubos',
       version='0.1',
       py_modules=['supercubos'],
-      ext_modules = cythonize("supercubos/csampling.pyx")
+      ext_modules = cythonize(extension, include_path=[numpy.get_include()]),
+      include_dirs=[numpy.get_include()],
 )
